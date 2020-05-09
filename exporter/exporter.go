562bd300ff56a2889acfe80fd29ddf97e54140ca
Patrick Hahn <patricksilashahn@gmx.net>
2020-05-09 14:43:10 +0200

    support reloading config files with SIGUSR1, ensure connection to MongoDB is actually there (using MongoDB Ping)

--- exporter/exporter.go
+++ exporter/exporter.go
@@ -11,6 +11,19 @@ import (
 
 type Exporter struct {
 	aggregations []prometheus.Collector
+	operations   chan aggregationOperation
+}
+
+type aggregationOperationType uint64
+
+const (
+	get aggregationOperationType = iota
+	reload
+)
+
+type aggregationOperation struct {
+	operationType aggregationOperationType
+	result        chan prometheus.Collector
 }
 
 func NewExporter(glob string, client *mongo.Client) (*Exporter, error) {
@@ -19,27 +32,64 @@ func NewExporter(glob string, client *mongo.Client) (*Exporter, error) {
 		return nil, fmt.Errorf("could not glob for aggregation files. glob=%s", glob)
 	}
 
-	var ags []prometheus.Collector
-
-	for _, match := range matches {
-		ag, err := aggregations.FromFile(match, client)
-		if err != nil {
-			log.Printf("could not parse aggregation file. filename=%s", match)
-			continue
+	exporter := &Exporter{aggregations: nil, operations: make(chan aggregationOperation)}
+	load := func() {
+		var ags []prometheus.Collector
+		for _, match := range matches {
+			ag, err := aggregations.FromFile(match, client)
+			if err != nil {
+				log.Printf("could not parse aggregation file. filename=%s", match)
+				continue
+			}
+			ags = append(ags, ag)
 		}
-		ags = append(ags, ag)
+		exporter.aggregations = ags
 	}
-	return &Exporter{aggregations: ags}, nil
+	load()
+
+	go func() {
+		for {
+			operation := <-exporter.operations
+			switch operation.operationType {
+			case get:
+				for _, elem := range exporter.aggregations {
+					operation.result <- elem
+				}
+				close(operation.result)
+			case reload:
+				load()
+			}
+		}
+	}()
+	return exporter, nil
+}
+
+func (e *Exporter) get() chan prometheus.Collector {
+	result := make(chan prometheus.Collector)
+	e.operations <- aggregationOperation{
+		operationType: get,
+		result:        result,
+	}
+	return result
 }
 
 func (e *Exporter) Describe(ch chan<- *prometheus.Desc) {
-	for _, ag := range e.aggregations {
+	result := e.get()
+	for ag := range result {
 		ag.Describe(ch)
 	}
 }
 
 func (e *Exporter) Collect(ch chan<- prometheus.Metric) {
-	for _, ag := range e.aggregations {
+	result := e.get()
+	for ag := range result {
 		ag.Collect(ch)
+	}
+}
+
+func (e *Exporter) Reload() {
+	e.operations <- aggregationOperation{
+		operationType: reload,
+		result:        nil,
 	}
 }
--- main.go
+++ main.go
@@ -7,10 +7,14 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 	"log"
 	"mongodb-query-exporter/aggregations"
 	exporter2 "mongodb-query-exporter/exporter"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
@@ -25,19 +29,20 @@ func main() {
 	client := connectToMongoDb()
 
 	exporter, err := exporter2.NewExporter(*aggregationPath, client)
-
 	if err != nil {
 		log.Println(err)
 	}
 	prometheus.MustRegister(exporter)
 	prometheus.MustRegister(aggregations.ExecutionHistogram)
 
+	signalListener(exporter)
+
 	log.Println("MongoDB query exporter starting on " + *addr + ", exporting metrics on /metrics")
 	log.Fatal(http.ListenAndServe(*addr, nil))
 }
 
 func connectToMongoDb() *mongo.Client {
-	log.Printf("Connection to MongoDB")
+	log.Printf("Connecting to MongoDB")
 	client, err := mongo.NewClient(options.Client().ApplyURI(*mongoDbUri))
 	if err != nil {
 		log.Fatalf("could not create mongodb client. reason=%v", err)
@@ -48,6 +53,24 @@ func connectToMongoDb() *mongo.Client {
 	if err != nil {
 		log.Fatalf("could not connect to mongodb. reason=%v", err)
 	}
+	ctx, _ = context.WithTimeout(context.Background(), 5*time.Second)
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		log.Fatalf("Tried to establish connection to mongodb, but failed: reason=%v", err)
+	}
 	log.Printf("Connected to MongoDB")
 	return client
+}
+
+func signalListener(exporter *exporter2.Exporter) {
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, syscall.SIGUSR1)
+
+	go func() {
+		for {
+			<-signals
+			log.Printf("Reloading config")
+			exporter.Reload()
+		}
+	}()
 }
